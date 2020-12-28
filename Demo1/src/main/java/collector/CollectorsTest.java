9dcc49170a422e4e0b3915146e588b19056d338a
wjtf <2644970580@qq.com>
2020-12-28 21:23:03 +0800

    CollectorsTest练习

--- Demo1/src/main/java/collector/CollectorsTest.java
+++ Demo1/src/main/java/collector/CollectorsTest.java
@@ -1,4 +1,21 @@
 package collector;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
 public class CollectorsTest {
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("111", "2222", "333333333");
+        toList(list);
+    }
+
+    private static void toList(List<String> list) {
+        List<String> collect = list.stream().collect(Collectors.toList());
+        System.out.println(collect);
+    }
 }
