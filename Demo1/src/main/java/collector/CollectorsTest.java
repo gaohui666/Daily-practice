c439dde64d7498b7d036f7f8bf245e8a76e4c942
wjtf <2644970580@qq.com>
2020-12-28 22:08:05 +0800

    练习

--- Demo1/src/main/java/collector/CollectorsTest.java
+++ Demo1/src/main/java/collector/CollectorsTest.java
@@ -1,22 +1,42 @@
 package collector;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toList;
-
 public class CollectorsTest {
 
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("111", "2222", "333333333", "444444");
+        List<String> list = Arrays.asList("111", "2222", "333333333", "444444", "drtferwf3422");
 
-        toList(list);
+//        toList(list);
+
+//        joiningTest(list);
+
+        mapingTest(list);
     }
 
     private static void toList(List<String> list) {
         List<String> collect = list.stream().collect(Collectors.toList());
         System.out.println(collect);
     }
+
+    public static void joiningTest(List<String> list){
+        // 无参方法
+        String s = list.stream().collect(Collectors.joining());
+        System.out.println(s);
+        // 指定连接符
+        String ss = list.stream().collect(Collectors.joining("-"));
+        System.out.println(ss);
+        // 指定连接符和前后缀
+        String sss = list.stream().collect(Collectors.joining("-","S","E"));
+        System.out.println(sss);
+    }
+
+    public static void mapingTest(List<String> list){
+        List<Integer> ll = list.stream().limit(4).collect(Collectors.mapping(Integer::valueOf,Collectors.toList()));
+        System.out.println(ll);
+    }
+
+
 }
